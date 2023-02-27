57ec5835383469facb9714e3691c0334e99c3c96
Jessica Nash <janash@vt.edu>
2023-02-26 21:13:34 -0500

    moves creation of PCA indices in script

--- DB_restructuring_queries.sql
+++ DB_restructuring_queries.sql
@@ -5,8 +5,6 @@ CREATE EXTENSION IF NOT EXISTS cube;
 CREATE INDEX IF NOT EXISTS idx_molecule_molecule_id ON molecule (molecule_id);
 CREATE INDEX IF NOT EXISTS idx_molecule_smiles ON molecule (smiles);
 CREATE INDEX IF NOT EXISTS idx_molecule_umap_knn ON molecule USING gist (umap);
-CREATE INDEX IF NOT EXISTS idx_molecule_pca ON molecule (pca);
-CREATE INDEX IF NOT EXISTS idx_molecule_pca_knn ON molecule USING gist (pca);
 CREATE INDEX IF NOT EXISTS idx_pca_smiles ON pca (smiles);
 
 ---- Set column pca in table molecule to a cube of all 4 pca component columns ----
@@ -15,6 +13,10 @@ UPDATE molecule
 SET pca = cube(ARRAY[pca.pca1, pca.pca2, pca.pca3, pca.pca4])
 FROM pca
 WHERE molecule.smiles = pca.smiles;
+
+--- Create indices on PCA ---
+CREATE INDEX IF NOT EXISTS idx_molecule_pca ON molecule (pca);
+CREATE INDEX IF NOT EXISTS idx_molecule_pca_knn ON molecule USING gist (pca);
 
 ---- Query to make umap into cube data type on molecule table ----
 ALTER TABLE molecule RENAME umap TO umap_point;
