4f75eeb6631159b0dc77d09ddcf260a4ab281591
Usman Jamshed <usman.jamshedk@gmail.com>
2023-03-21 03:10:41 -0400

    Adding test that should error to status code 400 or 500

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -69,6 +69,66 @@ def test_retrieve_dimensions():
 
 # TODO:
 # Failing tests (status_code == 400)
+# TODO: Set up catch on backend if the molecule id is out of range.
+def test_retrieve_molecule_under_range():
+
+    response = get_molecule(-1)
+    assert response.status_code == 500
+
+
+def test_retrieve_molecule_over_range():
+
+    response = get_molecule(400000)
+    assert response.status_code == 500
+
+
+def test_retrieve_substructure_search_invalid_smiles():
+
+    response = get_molecule_substructure_search("C=XX")
+    assert response.status_code == 400
+
+
+def test_retrieve_molecule_neighbors_invalid_type():
+
+    response = get_molecule_neighbors(1, "wrong_type", "1, 2, 3")
+    assert response.status_code == 400
+
+
+def test_retrieve_molecule_neighbors_invalid_components():
+
+    response = get_molecule_neighbors(1, "pca", "1, 2, 3, 4, 5")
+    assert response.status_code == 400
+
+
+def test_retrieve_molecule_neighbors_invalid_components_2():
+
+    response = get_molecule_neighbors(1, "umap", "4")
+    assert response.status_code == 400
+
+
+def test_retrieve_dimensions_invalid_type():
+
+    response = get_dimensions("wrong_type", "", "1, 2, 3")
+    assert response.status_code == 400
+
+
+def test_retrieve_dimensions_invalid_category():
+
+    response = get_dimensions("pca", "wrong_category", "1, 2, 3")
+    assert response.status_code == 400
+
+
+def test_retrieve_dimensions_invalid_components():
+
+    response = get_dimensions("pca", "", "1, 2, 3, 4, 5")
+    assert response.status_code == 400
+
+
+def test_retrieve_dimensions_invalid_components_2():
+
+    response = get_dimensions("umap", "", "5")
+    assert response.status_code == 400
+    
 # Output tests (correct number of items returned)
 # Output tests (correct components returned)
 # Output tests (correct values returned)
