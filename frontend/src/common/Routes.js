c923b6ea6c6d6451e4213ae4700385861ce8166f
Usman Jamshed <usman.jamshedk@gmail.com>
2023-05-30 18:07:16 -0400

    Added new api endpoint to retrieve molecule identifiers from smiles

--- frontend/src/common/Routes.js
+++ frontend/src/common/Routes.js
@@ -3,12 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import MoleculeInfo from '../pages/Molecule';
 
 
-  export const AppRoutes = ({ pages }) => (
-    <Routes>
-      <Route path='/' element={pages['Home']}></Route>
-      {Object.keys(pages).map((page, index) => (
-        <Route key={index} path={`/${page.toLowerCase()}`} element={pages[page]} />
-      ))}
-      <Route path="molecule/:molid" element={<MoleculeInfo />} />
-    </Routes>
-  );
+export const AppRoutes = ({ pages }) => (
+  <Routes>
+    <Route path='/' element={pages['Home']}></Route>
+    {Object.keys(pages).map((page, index) => (
+      <Route key={index} path={`/${page.toLowerCase()}`} element={pages[page]} />
+    ))}
+    <Route path="molecule/:molid" element={<MoleculeInfo />} />
+  </Routes>
+);
