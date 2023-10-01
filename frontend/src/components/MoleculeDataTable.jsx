b1dcfb17bbf4aac89a516c95f2077815fc2a2be8
Jessica Nash <janash@vt.edu>
2023-10-01 18:14:48 -0400

    molecule data table with switch (no download)

--- frontend/src/components/MoleculeDataTable.jsx
+++ frontend/src/components/MoleculeDataTable.jsx
@@ -1,12 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TablePagination from '@mui/material/TablePagination';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import React, { useEffect, useState } from 'react';
+import { DataGrid, GridFooterContainer, GridFooter } from "@mui/x-data-grid";
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { Select, MenuItem } from '@mui/material';
+import Button from '@mui/material/Button';
+
+
+const dataTypeMapping = {
+    "ML Data": "ml",
+    "DFT Data": "dft",
+    "XTB Data": "xtb",
+    "XTB_NI Data": "xtb_ni"
+};
 
 
 async function retrieveData(molecule_id, data_type) {
@@ -15,49 +20,99 @@ async function retrieveData(molecule_id, data_type) {
         const data = await response.json();
         return data;
     } catch (error) {
-        debugger;
         console.log(error);
         return null;
     }
 }
 
-export default function MoleculeDataTable({molecule_id, initial_data_type}) {
+function CustomFooter({ selectedDataType, setSelectedDataType }) {
+    const handleChange = (event) => {
+      setSelectedDataType(event.target.value);
+    };
+  
+    return (
+      <GridFooterContainer>
+        <Typography sx={{ color: 'gray', display: 'inline-block', verticalAlign: 'middle' }}>
+          Data Type:
+        </Typography>
+        <Select
+          value={selectedDataType}
+          onChange={handleChange}
+          displayEmpty
+          sx={{ marginLeft: '8px', marginRight: '16px', display: 'inline-block', verticalAlign: 'middle' }}
+        >
+          <MenuItem value="ML Data">ML Data</MenuItem>
+          <MenuItem value="DFT Data">DFT Data</MenuItem>
+          <MenuItem value="XTB Data">XTB Data</MenuItem>
+          <MenuItem value="XTB_NI Data">XTB_NI Data</MenuItem>
+        </Select>
+        <GridFooter sx={{
+          border: 'none', // To delete double border.
+        }} />
+      </GridFooterContainer>
+    );
+  }
+  
+  
+
+export default function MoleculeDataTable({ molecule_id, initial_data_type }) {
     const [moleculeData, setMoleculeData] = useState(null);
     const [data_type, setDataType] = useState(initial_data_type);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-    const [page, setPage] = useState(0);
+    const [selectedDataType, setSelectedDataType] = useState("ML Data"); // Set the default value to "ML Data"
 
     useEffect(() => {
         async function fetchData() {
-            const data = await retrieveData(molecule_id, data_type);
+            let data = await retrieveData(molecule_id, dataTypeMapping["DFT Data"]);
+    
+            // If DFT data is empty, default to ML Data
+            if (!data || Object.keys(data).length === 0) {
+                setSelectedDataType("ML Data");
+                data = await retrieveData(molecule_id, dataTypeMapping["ML Data"]);
+            } else {
+                setSelectedDataType("DFT Data");
+            }
             setMoleculeData(data);
         }
-
-        fetchData();
-    }, [data_type, molecule_id]);
     
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="molecule_data_table" stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell>Property</TableCell>
-            <TableCell align="right">Value</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {moleculeData && 
-        Object.keys(moleculeData)
+        fetchData();
+    }, [molecule_id]);
+
+    useEffect(() => {
+        setDataType(dataTypeMapping[selectedDataType]);
+        console.log(data_type)
+    }, [selectedDataType]);
+
+    const columns = [
+        { field: 'property', headerName: 'Property', filterable: true, flex: true },
+        { field: 'value', headerName: 'Value', width: 150, filterable: true, headerAlign: 'right', align: 'right', flex: true }
+    ];
+
+    const rows = moleculeData ? Object.keys(moleculeData)
         .filter(key => key !== 'smiles' && key !== 'molecule_id')
-        .map((key) => (
-            
-            <TableRow key={key}>
-                <TableCell align="left">{key}</TableCell>
-                <TableCell align="right">{moleculeData[key]}</TableCell>
-            </TableRow>
-            ))};
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+        .map(key => ({
+            id: key,
+            property: key,
+            value: moleculeData[key],
+        })) : [];
+
+    return (
+        <Paper elevation={3} style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                components={{Footer: CustomFooter}}
+                componentsProps={{
+                    footer: { selectedDataType, setSelectedDataType }
+                }}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 25, // This sets the initial page size
+                        },
+                    },
+                }}
+                pageSizeOptions={[5, 10, 25, 50, 100]} 
+            />
+        </Paper>
+    );
 }
